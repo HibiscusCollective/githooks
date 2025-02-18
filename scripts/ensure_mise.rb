135a7c9e5f992603fb009491bfebe56a938b1c2d
pfouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-02-18 20:23:40 +0800

    feat(scripts): remove ruby scripts

--- scripts/ensure_mise.rb
+++
@@ -1,8 +0,0 @@
-#!ruby
-# frozen_string_literal: true
-
-unless system('command -v mise >/dev/null 2>&1')
-  abort('mise is not installed, please visit https://mise.jdx.dev/getting-started.html for installation instructions')
-end
-
-abort('failed to run mise install, tools may not be properly set up') unless system('mise install')
