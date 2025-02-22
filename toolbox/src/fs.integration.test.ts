8fc74124599f62e3f2e3657a35464991f08251f9
pfouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-02-22 08:52:23 +0800

    feat(toolbox): implement ls command

---
+++ toolbox/src/fs.integration.test.ts
@@ -0,0 +1,46 @@
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from 'bun:test'
+
+import type { PathLike } from 'fs'
+import * as fs from 'fs'
+import * as path from 'path'
+
+import { AsyncFS, DirInfo, FileInfo } from './fs'
+
+const TMP_DIR = 'tmp'
+const INTEGRATION_TEST_DIR = path.join(TMP_DIR, 'integration')
+
+beforeAll(() => {
+	fs.promises.mkdir(INTEGRATION_TEST_DIR, { recursive: true })
+})
+
+describe('given a temporary directory', async () => {
+	const tmpDirPrefix = 'fs_integration_test_'
+	let tmpDir: PathLike
+
+	beforeEach(async () => {
+		tmpDir = await fs.promises.mkdtemp(tmpDirPrefix)
+		expect(tmpDir).toBeDefined()
+	})
+
+	afterEach(async () => {
+		await fs.promises.rm(tmpDir, { recursive: true, force: true })
+		expect(await fs.promises.exists(tmpDir)).toBeFalse()
+	})
+
+	describe('and a testdata directory', async () => {
+		const testdataDir = path.join(__dirname, 'testdata')
+		expect(await fs.promises.exists(testdataDir)).toBeTrue()
+
+		describe('when the ls command is invoked on the testdata directory', async () => {
+			const files = await new AsyncFS().ls(testdataDir)
+
+			it('it should return a list of files and directories in the testdata directory', () => {
+				expect(files).toContainAllValues([
+					new DirInfo(path.join(testdataDir, 'subdir')),
+					new FileInfo(path.join(testdataDir, 'test.txt')),
+					new FileInfo(path.join(testdataDir, 'empty.txt')),
+				])
+			})
+		})
+	})
+})
---
+++ toolbox/src/fs.ts
@@ -0,0 +1,75 @@
+import type { PathLike } from 'fs'
+import * as fs from 'fs'
+import * as path from 'path'
+import { ok, type Result } from './result'
+
+export interface LsOptions {
+	readonly recursive: boolean
+	readonly files: boolean
+	readonly directories: boolean
+}
+
+export interface FileLike {
+	readonly path: PathLike
+	readonly isDirectory: boolean
+}
+
+export interface FSLike {
+	ls: (path: PathLike, options?: LsOptions) => Promise<FileLike[]>
+	cp: (source: PathLike, dest: PathLike) => Promise<Result<PathLike>>
+	mkdirp(p: PathLike): Promise<Result<PathLike>>
+}
+
+export class FileInfo implements FileLike {
+	readonly path: PathLike
+	readonly isDirectory: false
+
+	constructor(path: PathLike) {
+		this.path = path
+		this.isDirectory = false
+	}
+}
+
+export class DirInfo implements FileLike {
+	readonly path: PathLike
+	readonly isDirectory: true
+
+	constructor(path: PathLike) {
+		this.path = path
+		this.isDirectory = true
+	}
+}
+
+const DEFAULT_LS_OPTS: LsOptions = { recursive: false, files: true, directories: true }
+
+export class AsyncFS implements FSLike {
+	async ls(p: PathLike, options: LsOptions = DEFAULT_LS_OPTS): Promise<FileLike[]> {
+		const files = await fs.promises.readdir(p)
+
+		const infos = await Promise.all(
+			files.map(async file => {
+				const filePath = path.resolve(p.toLocaleString(), file)
+				const stat = await fs.promises.stat(filePath)
+
+				return stat.isDirectory() ? new DirInfo(filePath) : new FileInfo(filePath)
+			})
+		)
+
+		return infos
+	}
+
+	async cp(source: PathLike, dest: PathLike): Promise<Result<PathLike>> {
+		return ok(dest)
+	}
+
+	async mkdirp(p: PathLike): Promise<Result<PathLike>> {
+		const abs = await this.absPath(p)
+		return abs
+	}
+
+	private async absPath(p: PathLike): Promise<Result<PathLike>> {
+		const absPath = path.resolve(p.toLocaleString())
+
+		return ok(absPath)
+	}
+}
