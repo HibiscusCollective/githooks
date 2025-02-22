0ada7af7af65082d8a155fc108a25dde8b954580
pfouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-02-22 08:05:07 +0800

    feat(toolbox): add extension methods to result type

--- toolbox/src/result.ts
+++ toolbox/src/result.ts
@@ -8,19 +8,23 @@ export class StructuredError extends Error {
 	}
 }
 
-export function ok<T, E extends StructuredError = StructuredError>(value: T): Result<T, E> {
-	return new Result<T, E>(value, undefined)
+export function ok<E extends StructuredError = StructuredError>(): Result<void, E>
+export function ok<T, E extends StructuredError = StructuredError>(value: T): Result<T, E>
+export function ok<T, E extends StructuredError = StructuredError>(value?: T): Result<T | void, E> {
+	return new Result<T | void, E>(value, undefined)
 }
 
-export function err<T, E extends StructuredError = StructuredError>(error: E): Result<T, E> {
+export function err<E extends StructuredError = StructuredError>(): Result<void, E>
+export function err<T, E extends StructuredError = StructuredError>(error: E): Result<T, E>
+export function err<T, E extends StructuredError = StructuredError>(error?: E): Result<T, E> {
 	return new Result<T, E>(undefined, error)
 }
 
-export class Result<T, E extends StructuredError = StructuredError> {
+export class Result<T = void, E extends StructuredError = StructuredError> {
 	readonly value: T | undefined
 	readonly error: E | undefined
 
-	constructor(value: T | undefined, error: E | undefined) {
+	constructor(value?: T, error?: E) {
 		this.value = value
 		this.error = error
 	}
